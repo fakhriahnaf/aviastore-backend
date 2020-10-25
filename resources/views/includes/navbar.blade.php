97df17764d72b0b1abd20ae98d5583e084ffb3b1
fakhri ahnaf <fakhri_ahnaf01@yahoo.co.id>
2020-10-25 17:24:11 +0700

    push

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -15,8 +15,16 @@
                     <img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar">
                 </a>
 
-                <div class="user-menu dropdown-menu">
-                    <a class="nav-link" href="#"><i class="fa fa-power -off"></i>Logout</a>
+                <div class="user-menu dropdown-menu" >
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-jet-dropdown-link href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                            {{ __('Logout') }}
+                        </x-jet-dropdown-link>
+                    </form>
                 </div>
             </div>
 
